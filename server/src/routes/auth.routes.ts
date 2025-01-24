126306b65dd465ff830b69d5ceefd3105a6a7a66
IDRISSI O. Ayoub <idrissio.ayoub@gmail.com>
2025-01-24 17:38:19 +0100

    feat: jwt logic

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,17 +1,28 @@
 import express from "express";
-import { hashPassword } from "../middlewares/auth.middleware";
+import {comparePassword, hashPassword} from "../middlewares/argon.middleware";
 import {add} from "../modules/user/userActions";
+import {getUserByEmail} from "../middlewares/user.middleware";
+import {login} from "../modules/auth/authActions";
 
 const router = express.Router();
 
-// 1 - Vérifier l'intégrité des données - MIDDLEWARE
+// Je récupère les données du front
+// req.body
 
-// 3 - Hash du mot de passe - MIDDLEWARE
+// Je hash le mot de passe
+// -> hashPassword
 
-// 2 - Vérifier si l'email n'est pas déja utilisé - MIDDLEWARE
+// Vérifie si l'utilisateur existe
+// getUserByEmail
+// S'il existe, on ajoute une nouvelle clé dans le req.body pour pouvoir comparer les mots de passe (formulaire / base de données)
 
-// router.post("/register", validateAuthRegister, hashPassword, checkUniqueEmail, authRegister);
+// compare les mots de passe
+// comparePassword
+
+// Autoriser l'authentification
+// ->
 
 router.post("/register", hashPassword, add);
+router.post("/login", getUserByEmail, comparePassword, login)
 
 export default router;
